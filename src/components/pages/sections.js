61d67890484355af301a695cd9584ee2ac316910
unknown <robinkaris7@gmail.com>
2022-02-24 11:46:04 +0300

    added sections page

---
+++ src/components/pages/sections.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import ListGroup from 'react-bootstrap/ListGroup';
+
+
+const Sections = () => {
+  return (
+
+    <ListGroup variant="flush" className='mt-5'>
+  <ListGroup.Item >Main Entrance</ListGroup.Item>
+  <ListGroup.Item>FMCG</ListGroup.Item>
+  <ListGroup.Item>GM</ListGroup.Item>
+  <ListGroup.Item>Furniture</ListGroup.Item>
+  <ListGroup.Item>Fresh</ListGroup.Item>
+  <ListGroup.Item>Receiving</ListGroup.Item>
+  </ListGroup>
+  
+  )
+}
+
+export default Sections
