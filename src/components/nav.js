65165d4e9d69868ad5531b195b2cdf28ee69729f
unknown <robinkaris7@gmail.com>
2022-07-18 11:28:51 +0300

    made changes

--- src/components/nav.js
+++ src/components/nav.js
@@ -33,7 +33,7 @@ const Navigation = ({token}) => {
     <Navbar.Brand>Naivas</Navbar.Brand>
     <Nav className="me-auto">
       
-      <Nav.Link as={Link} to="/">Home</Nav.Link>
+      <Nav.Link as={Link} to="/">Questions</Nav.Link>
       <Nav.Link as={Link} to="/Pending">Pending</Nav.Link>
   
     </Nav>
